95fc35d091da473a6d5f70a8f876de51937c73d8
Jazzik Peng <zhejianpeng@yahoo.com>
2018-05-28 14:35:49 -0500

    'add SPAN_Explanation.pdf'

--- VaR.py
+++ VaR.py
@@ -1,3 +1,14 @@
-print('Hello')
+# coding: utf-8
+'''
+__author__ = "Zhejian Peng"
+__copyright__ = "Zhejian Peng"
+__license__ = MIT LICENSE
+__version__ = "1.0.1"
+__maintainer__ = "Zhejian Peng"
+__email__ = "zhejianpeng@yahoo.com"
+__status__ = "Developing"
 
-print('World')
+__update__ =
+'''
+import numpy as np
+import pandas as pd
